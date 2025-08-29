d5e594070598c7a46547cabf8ef199aebe668b6d
Fyuk11 <rodrigomez.11e@gmail.com>
2025-08-29 11:11:25 -0300

    Actualización de estilos

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ lines.forEach((line, index) => {
   }, index * 1200); // espera entre líneas
 });
 */
-
+/*
 const lines = document.querySelectorAll('.type-line');
 
 lines.forEach((line, index) => {
@@ -63,8 +63,26 @@ lines.forEach((line, index) => {
   }, index * 1200); // delay entre líneas
 });
 
+*/
+const lines = document.querySelectorAll('.type-line');
 
+lines.forEach((line, index) => {
+  const text = line.getAttribute('data-full');
+  line.textContent = '';
+  let i = 0;
 
+  setTimeout(() => {
+    line.classList.add('typing'); // cursor mientras escribe
+    const interval = setInterval(() => {
+      line.textContent += text[i];
+      i++;
+      if (i >= text.length) {
+        clearInterval(interval);
+        line.classList.remove('typing'); // quita cursor al terminar
+      }
+    }, 50);
+  }, index * 1200); // delay entre líneas
+});
 
 // Swiper Proyectos
 
